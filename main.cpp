e50a56eeb403315373deed2d469391865511dc81
Otsuka Daichi <109046865+KujataD@users.noreply.github.com>
2026-04-16 14:18:39 +0900

    修正スペルミス検出可能

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// 出力ウィンドウへの文字出力
-	OutputDebugStringA("Hello,DirectX!\n");
+	int numbesr = 1;
+	if (numbesr == 1) {
+		OutputDebugStringA("Hello,DirectX!\n");
+	}
 
 	return 0;
 }
