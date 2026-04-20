68e625f27aa467e483e11d2b4c96b3e263849203
Otsuka Daichi <109046865+KujataD@users.noreply.github.com>
2026-04-20 14:00:12 +0900

    修正、テスト用変数削除

--- main.cpp
+++ main.cpp
@@ -3,10 +3,7 @@
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// 出力ウィンドウへの文字出力
-	int numbesr = 1;
-	if (numbesr == 1) {
-		OutputDebugStringA("Hello,DirectX!\n");
-	}
+	OutputDebugStringA("Hello,DirectX!\n");
 
 	return 0;
 }
